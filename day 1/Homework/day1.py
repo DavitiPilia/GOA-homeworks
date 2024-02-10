bb5cb2d15d3f2e8ff511bfdd1770eef2cb7bf6e3
DavitPilia <159659388+DavitiPilia@users.noreply.github.com>
2024-02-11 02:58:08 +0400

    Add files via upload

---
+++ day 1/Homework/day1.py
@@ -0,0 +1,7 @@
+name = "Daviti"
+surname = "Philia"
+height = "1.85"
+age = "14"
+My_Information = "I  am  Daviti  Philia,  I  am  14  years  old  and  i  am  1.85."
+print(name + "  " + str(surname) + "  " + str(age) + "  " + str(1.85) )
+print(My_Information)
